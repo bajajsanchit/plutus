5dd3b0d66ddd4040b1c1a514764a23ecf29bc301
bajajsanchit <65711927+bajajsanchit@users.noreply.github.com>
2024-07-20 21:12:39 +0530

    app mockup container

---
+++ src/components/AppMockup/index.js
@@ -0,0 +1,28 @@
+//styles
+import styles from "./styles.module.scss";
+
+const AppMockup = () => {
+	return (
+		<div className={styles.parent}>
+			<div className={styles.container}>
+				<div className={styles.image}>
+					<img src={"/images/plutus-mockup-2.png"} />
+				</div>
+
+				<div className={styles.content}>
+					<p className={styles.heading_sm}>Your Financial Ally</p>
+					<p className={styles.text}>The Credit Card Experience</p>
+					<p className={styles.heading_lg}>
+						Plutus revolutionizes the way you manage your credit cards. Compare
+						top offers, maximize rewards, and access exclusive deals tailored to
+						your needs. Join Plutus today and take control of your financial
+						future with confidence.
+					</p>
+					<p className={styles.heading_md}>Get Early Access</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default AppMockup;
